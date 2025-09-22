22122cdb24cd67b28926077af6428afabf4a62d7
Arno Massart <arno.massart@outlook.com>
2025-09-22 10:53:40 +0200

    Added a platform to the sphere with colliders for walls.

--- Assets/Scripts/Teleport.cs
+++ Assets/Scripts/Teleport.cs
@@ -5,7 +5,11 @@ public class Teleport : MonoBehaviour
 {
     [Header("Teleport Settings")]
     public Transform teleportTarget;   
-    public GameObject playerRig;      
+    public GameObject playerRig;
+
+    [Header("Room Settings")]
+    [Tooltip("Assign all objects from the room that should disappear after teleporting.")]
+    public GameObject[] roomObjects;
 
     private void OnTriggerEnter(Collider other)
     {
@@ -15,6 +19,17 @@ public class Teleport : MonoBehaviour
             Debug.Log("teleport");
             playerRig.transform.position = teleportTarget.position;
             playerRig.transform.rotation = teleportTarget.rotation;
+
+            DisableRoom();
+        }
+    }
+
+    private void DisableRoom()
+    {
+        foreach (GameObject obj in roomObjects)
+        {
+            if ( obj != null)
+                obj.SetActive(false);
         }
     }
 }
